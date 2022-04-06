ee7857848f8165bf00563edef186c2b57b8c0f1f
hitrandil <mattiamine00@gmail.com>
2022-04-06 16:41:36 +0200

    [init] First commit

---
+++ main.py
@@ -0,0 +1,115 @@
+# import bs4, requests, webbrowser
+
+# FIDAL_VA129 = 'https://www.fidal.it/societa/Pol--Olonia/VA129'
+# PRE_ATLETA = 'https://www.fidal.it/atleta/'
+# ATLETA_ESEMPIO = 'https://www.fidal.it/atleta/Matilde-Silanos/drKRk5WncGU'
+
+# response = requests.get(ATLETA_ESEMPIO) #ottiene la pagina html
+# response .raise_for_status() #genera eccezione se la richiesta non è andata a buon fine
+# soup = bs4.BeautifulSoup(response.text, 'html.parser'); #salvo in soup l'html della pagina
+# div_competions = soup.find('div', class_='table-responsive') #trova tutti i div delle diverse discipline
+
+# result_gare = div_competions.find_all('form')
+# result_data = []
+# for result_gare in result_gare:
+#     result_data = str(div_competions.get(''))
+
+
+import requests, re, json, mysql.connector, os
+from dotenv import load_dotenv
+from bs4 import BeautifulSoup
+from mysql.connector import Error
+
+conn = None
+def connect():
+    """ Connect to MySQL database """
+    try:
+        conn = mysql.connector.connect(host=os.getenv("HOST"),
+                                       database=os.getenv("NAME_DB"),
+                                       user=os.getenv("USER_DB"),
+                                       password=os.getenv("PSW_DB"))
+        if conn.is_connected():
+            print('Connected to MySQL database')
+            return(conn)
+
+    except Error as e:
+        print(e)
+    
+
+def insert(datoGara):
+    mycursor = conn.cursor()
+    if 'dataNascita' in datoGara:
+        sql = "INSERT INTO prova (nome_cognome, data_nascita, data_prestazione, disciplina, prestazione, societa_prestazione) VALUES (%s, STR_TO_DATE(%s, '%d-%m-%Y'), STR_TO_DATE(%s, '%d/%m/%Y'), %s, %s, %s)"
+        val = (datoGara['nomeCognome'], datoGara['dataNascita'], datoGara['data'], datoGara['disciplina'], datoGara['prestazione'], datoGara['societaPrestazione'])
+    else:
+        sql = "INSERT INTO prova (nome_cognome, data_prestazione, disciplina, prestazione, societa_prestazione) VALUES (%s,  STR_TO_DATE(%s, '%d/%m/%Y'), %s, %s, %s)"
+        val = (datoGara['nomeCognome'], datoGara['data'], datoGara['disciplina'], datoGara['prestazione'], datoGara['societaPrestazione'])
+    mycursor.execute(sql, val)
+
+    conn.commit()
+
+    print(mycursor.rowcount, "record inserted.")
+
+def getLinkAtleti(urlSocieta):
+    listAtleti = []
+    PREFIX_ATL = "https://www.fidal.it/atleta/"
+    SUFF_SOC = "#tab2"
+    
+    r = requests.get(urlSocieta)
+    soup = BeautifulSoup(r.text, 'html.parser')
+
+    for table in soup.find_all('table'):
+        for row in table.find_all('tr'):
+            for col in row.find_all('td'):
+                links = col.find_all('a')
+                if len(links)>0:
+                    if PREFIX_ATL in links[0]['href']:
+                        getDatiAtleta(links[0]['href'])
+                        listAtleti.append(links[0]['href'])
+    if SUFF_SOC in urlSocieta:
+        return listAtleti
+    return listAtleti + getLinkAtleti(urlSocieta + "#tab2")
+
+def salvaSuFile(dato):
+    fileJson = open('datiAtleti.json','a')
+    fileJson.write(json.dumps(dato))
+
+def getDatiAtleta(urlAtleta):
+    """Ottiene i dati dell'atleta presenti nella pagina Fidal dello stesso, non tutti gli atleti hanno la dataNascita
+    :param urlAtleta: url dell'atleta dal quale scaricare i dati 
+    """
+    date_extract_pattern = "[0-9]{1,2}\-[0-9]{1,2}\-[0-9]{4}"
+
+    r = requests.get(urlAtleta)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    atleta = []
+    datoGara = {}
+
+    datoGara["nomeCognome"] = soup.find_all('h1', limit = 1)[0].text
+    dataNascita = soup.find_all(style='float:left;')
+   
+    dataNascita = re.findall(date_extract_pattern, dataNascita[1].text)
+    if len(dataNascita)>0:
+        datoGara["dataNascita"] = dataNascita[0]
+    #todo tirare fuori società con link
+
+    #?estraggo dati gare
+    for table in soup.find_all('table', class_ ="table"):
+        if len(table.find_all('form', method="post"))>0:
+            for form in table.find_all('form', method="post"):
+                #todo aggiungere luogo prestazione
+                datoGara["data"] = form.find_all('input', attrs={"name": "titleDate"})[0]['value']
+                datoGara["disciplina"] = form.find_all('input', attrs={"name": "titleSportType"})[0]['value']
+                datoGara["prestazione"] = form.find_all('input', attrs={"name": "titleTime"})[0]['value'].strip()
+                datoGara["societaPrestazione"] = form.find_all('input', attrs={"name": "titleN"})[0]['value']
+                insert(datoGara)
+                #!print(datoGara)
+
+load_dotenv()
+conn = connect()
+getDatiAtleta("https://www.fidal.it/atleta/Paolo-Colombo/drKRkpWnaWs%3D")
+#*----------link utili----------------------------------
+#atleta senza data nascita: https://www.fidal.it/atleta/Paolo-Colombo/drKRkpWnaWs%3D
+#atleta con data nascita: https://www.fidal.it/atleta/Laura-Cortesi/drKRk5iobmw%3D
+#società: https://www.fidal.it/societa/POL--OLONIA/VA129
+#*-------------------------------------------------------
---
+++ readme.md
@@ -0,0 +1,63 @@
+# Fidal Data Scraping
+
+Il progetto nasce con l'intento di ricreare il database della Fidal lombardia popolato con tutti i dati già pubblici e consultabili in maniera libera sul sito <a href="https://fidal.it"> FIDAL Italia. </a>
+
+#### Il software è ancora in fase di sviluppo. 
+
+#### Consulta qua lo stato attuale del progetto:
+
+Dato l'url di una società, passata alla funzione `getLinkAtleti(urlSocieta)` la funzione recupera dalla pagina tutti i link degli atleti tesserati nell'anno corrente in quella società, man mano che trova i link li passa alla funzione `getDatiAtleta(urlAtleta)` la quale estrae e inserisce nel dict `datoGara[]` i seguenti dati:
+
+* Nome Cognome
+* Data Nascita (se presente)
+* Data prestazione
+* Disciplina
+* Prestazione
+* Società di tesseramento al momento della prestazione
+
+Per ogni prestazione trovata tutta questa stringa viene passata alla funzione `insert(dato)` la quale carica in maniera grezza i dati sul db.
+
+#### Miglioramenti logici nel codice:
+
+* Rendere versatili tutte le funzioni, costruire dizionario per ognuna e gestire l'intero dizionario come dato in ingresso.
+
+#### Todo:
+
+* Progettazione db e relativo caricamento corretto;Visual Studio Code
+* polivalenza nell'utilizzo, possibilità di salvare su file di diversi formati i dati prelevati dallo scraping;
+* recuperare i dati degli atleti *in pensione*, attualmente se un atleta non è tesserato in nessuna società non si riesce a recuperare la sua pagina, se non manualmente;
+* recuperare elenco di tutte le società italiane con relativo indirizzo link;
+* visualizzare stato di avanzamento del caricamento;
+
+<hr>
+
+### ~~Come usarlo?~~
+
+* Clonare la repository
+* Creare un DB (da zero oppure utilizzando il file `database.sql*`)  che rispetti il modello er
+* Modificare il file `.env` con la stessa struttura del `modello presente*`  nella repository inserendo gli estremi di accesso al database
+* Eseguire lo script `main.py` per popolare il database o per aggiornarlo
+
+##### ~~Automatizzare l'update del db (linux SO):~~
+
+* Inserire nel crontab la seguente stringa:
+
+### Open Source [![Unlicense](https://img.shields.io/badge/License-Unlicense-blue.svg)](https://unlicense.org/)
+
+Il software è interamente open source. 
+
+### Compatibilità - [![Linux](https://svgshare.com/i/Zhy.svg)](https://svgshare.com/i/Zhy.svg) [![Windows](https://svgshare.com/i/ZhY.svg)](https://svgshare.com/i/ZhY.svg) [![macOS](https://svgshare.com/i/ZjP.svg)](https://svgshare.com/i/ZjP.svg)
+
+Essendo scritto in python è compatibile ovunque questo venga installato. La guida per l'automatizzazione dello script è compatibile solamente con dispositivi Linux.
+
+### Contattami
+
+Mi puoi contattare su Telegram: <a href="https:t.me/hitrandil">@Hitrandil </a>
+
+### Credits
+
+Il software è pensato e creato da <a href = "https://github.com/Hitrandil/">@hitrandil </a>.
+
+<span style = "font-size: 10px;"> *i file contrassegnati con asterisco non sono ancora stati creati </span>
+
+[![made-with-python](https://img.shields.io/badge/Made%20with-Python-1f425f.svg)](https://www.python.org/)  [![Visual Studio Code](https://img.shields.io/badge/--007ACC?logo=visual%20studio%20code&logoColor=ffffff)](https://code.visualstudio.com/) [![Ask Me Anything !](https://img.shields.io/badge/Ask%20me-anything-1abc9c.svg)](https://GitHub.com/Naereen/ama)
